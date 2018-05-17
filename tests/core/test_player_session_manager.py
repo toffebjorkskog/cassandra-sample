58175a21c3be7015aca7f4e31d3207e3abbc8e38
Christoffer Björkskog <christoffer.bjorkskog@genero.fi>
2018-05-17 21:53:11 +0300

    add test for wrong timestamp

--- tests/core/test_player_session_manager.py
+++ tests/core/test_player_session_manager.py
@@ -11,3 +11,16 @@ def test_create_start_event():
             }
     returndata = insert_start_event(event)
     assert str(returndata["player_id"]) == "02a05113-7239-4597-aa76-c89f106aa547"
+
+
+def test_create_start_event_with_wrong_date():
+    from player_session_service.core.player_session_manager import insert_start_event
+    event = {
+                "player_id": "02a0511372394597aa76c89f106aa547",
+                "country": "AE",
+                "event": "start",
+                "session_id": "e0d836e6-2128-436b-a689-38c47a477118",
+                "ts": "2016-99-06T03:11:56"
+            }
+    with pytest.raises(Exception) as e_info:
+        returndata = insert_start_event(event)
