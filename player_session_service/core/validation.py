2cc35d695d2ee1b77201f64f3081f55c72bcd3f0
Christoffer Björkskog <christoffer.bjorkskog@genero.fi>
2018-05-17 22:54:42 +0300

    validate that start event has all attributes

--- player_session_service/core/validation.py
+++ player_session_service/core/validation.py
@@ -1,7 +1,7 @@
 def validate_start_event(event):
-    if 'event' in event.keys():
-        if event['event'] not in ['start', 'end']:
-            raise ValueError('Invalid event value, it should be "start" or "end"')
-    else:
-        raise TypeError('Missing attribute: event')
+    for key in ['event', 'player_id', 'country', 'session_id', 'ts']:
+        if key not in event.keys():
+            raise TypeError('Missing attribute: {}'.format(key))
 
+    if event['event'] not in ['start', 'end']:
+        raise ValueError('Invalid event attribute, allowed values are "start" and "end"')
