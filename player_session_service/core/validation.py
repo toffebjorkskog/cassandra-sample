c574cd99c32be433dcc6ac02b4ca282af9a46851
Christoffer Björkskog <christoffer.bjorkskog@genero.fi>
2018-05-17 22:58:34 +0300

    add validation tests

--- player_session_service/core/validation.py
+++ player_session_service/core/validation.py
@@ -1,3 +1,6 @@
+import dateutil.parser
+
+
 def validate_start_event(event):
     for key in ['event', 'player_id', 'country', 'session_id', 'ts']:
         if key not in event.keys():
@@ -5,3 +8,5 @@ def validate_start_event(event):
 
     if event['event'] not in ['start', 'end']:
         raise ValueError('Invalid event attribute, allowed values are "start" and "end"')
+
+    dateutil.parser.parse(event['ts'])  # naturally raises exception..
---
+++ tests/core/test_validation.py
@@ -0,0 +1,70 @@
+import pytest
+from player_session_service.core.validation import validate_start_event
+
+
+def test_validate_start_event__missing_parameters__event():
+    event = {
+            'player_id': '02a0511372394597aa76c89f106aa547',
+            'country': 'AE',
+            'session_id': 'e0d836e6-2128-436b-a689-38c47a477118',
+            'ts': '2016-11-06T03:11:56'
+    }
+    with pytest.raises(TypeError) as e_info:
+        validate_start_event(event)
+
+
+def test_validate_start_event__missing_parameters__player_id():
+    event = {
+            'country': 'AE',
+            'event': 'start',
+            'session_id': 'e0d836e6-2128-436b-a689-38c47a477118',
+            'ts': '2016-11-06T03:11:56'
+    }
+    with pytest.raises(TypeError) as e_info:
+        validate_start_event(event)
+
+
+def test_validate_start_event__missing_parameters__country():
+    event = {
+            'player_id': '02a0511372394597aa76c89f106aa547',
+            'event': 'start',
+            'session_id': 'e0d836e6-2128-436b-a689-38c47a477118',
+            'ts': '2016-11-06T03:11:56'
+    }
+    with pytest.raises(TypeError) as e_info:
+        validate_start_event(event)
+
+
+def test_validate_start_event__missing_parameters__session_id():
+    event = {
+            'player_id': '02a0511372394597aa76c89f106aa547',
+            'country': 'AE',
+            'event': 'start',
+            'ts': '2016-11-06T03:11:56'
+    }
+    with pytest.raises(TypeError) as e_info:
+        validate_start_event(event)
+
+
+def test_validate_start_event__missing_parameters__ts():
+    event = {
+            'player_id': '02a0511372394597aa76c89f106aa547',
+            'country': 'AE',
+            'event': 'start',
+            'session_id': 'e0d836e6-2128-436b-a689-38c47a477118',
+    }
+    with pytest.raises(TypeError) as e_info:
+        validate_start_event(event)
+
+
+def test_validate_start_event__wrong_parameters__ts():
+    event = {
+            'player_id': '02a0511372394597aa76c89f106aa547',
+            'country': 'AE',
+            'event': 'start',
+            'session_id': 'e0d836e6-2128-436b-a689-38c47a477118',
+            'ts': '2016-00-06T03:11:56'
+
+    }
+    with pytest.raises(ValueError) as e_info:
+        validate_start_event(event)
