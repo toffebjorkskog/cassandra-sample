14bfa8817b0fca93c6fd1bdf8dcd9e5023fe4759
Christoffer Björkskog <christoffer.bjorkskog@genero.fi>
2018-05-20 10:49:00 +0300

    update config system

--- player_session_service/__init__.py
+++ player_session_service/__init__.py
@@ -1,13 +1,21 @@
 from flask import Flask
 import os
+from config import TestingConfig, DevelopmentConfig, ProductionConfig
+
+config_options = {
+    "development": "config.DevelopmentConfig",
+    "testing": "config.TestingConfig",
+    "production": "config.ProductionConfig",
+}
 
 
 def create_app(KEYSPACE_OVERRIDE=None):
     from . import apis, core, models
     app = Flask(__name__)
 
-    # Load default settins
-    app.config.from_pyfile('default_settings.cfg')
+    # Load default settings
+    config_name = os.getenv('FLASK_ENV', 'development')
+    app.config.from_object(config_options[config_name])
 
     # Override defaults settings from file that env variable
     # PLAYER_SESSION_SERVICE_SETTINGS points to
@@ -15,7 +23,6 @@ def create_app(KEYSPACE_OVERRIDE=None):
         app.config.from_envvar('PLAYER_SESSION_SERVICE_SETTINGS')
 
     # Finally, allow keyspace to be overridden via app factory paramter
-    # This is used by pytest fo runittesting
     if KEYSPACE_OVERRIDE is not None:
         app.config['CASSANDRA_KEYSPACE'] = KEYSPACE_OVERRIDE
 
---
+++ player_session_service/config.py
@@ -0,0 +1,28 @@
+class BaseConfig(object):
+    DEBUG = True
+    TESTING = False
+    CASSANDRA_HOSTS = ['127.0.0.1']
+    CASSANDRA_KEYSPACE = 'player_sessions'
+    CASSANDRA_CONSISTENCY = 'QUORUM'
+    CASSANDRA_SETUP_KWARGS = {'protocol_version': 3}
+    CASSANDRA_LAZY_CONNECT = True   # True - should not connect until firstuse
+    CASSANDRA_RETRY_CONNEC = False  # True means reconnect even when failure
+    SECRET_KEY = 'qpwkedfvdöojfödskg'  # override this in production
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+    TESTING = True
+    CASSANDRA_KEYSPACE = 'player_sessions_development'
+
+
+class TestingConfig(BaseConfig):
+    DEBUG = False
+    TESTING = True
+    CASSANDRA_KEYSPACE = 'player_sessions_testing'
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
+    TESTING = False
+    CASSANDRA_KEYSPACE = 'player_sessions_production'
