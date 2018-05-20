20a1e1e09a9ce13309e6f0b51333fa9365d2f09a
Christoffer Björkskog <christoffer.bjorkskog@genero.fi>
2018-05-20 09:39:45 +0300

    document app factory

--- player_session_service/__init__.py
+++ player_session_service/__init__.py
@@ -6,10 +6,16 @@ def create_app(KEYSPACE_OVERRIDE=None):
     from . import apis, core, models
     app = Flask(__name__)
 
+    # Load default settins
     app.config.from_pyfile('default_settings.cfg')
+
+    # Override defaults settings from file that env variable
+    # PLAYER_SESSION_SERVICE_SETTINGS points to
     if 'PLAYER_SESSION_SERVICE_SETTINGS' in os.environ:
         app.config.from_envvar('PLAYER_SESSION_SERVICE_SETTINGS')
 
+    # Finally, allow keyspace to be overridden via app factory paramter
+    # This is used by pytest fo runittesting
     if KEYSPACE_OVERRIDE is not None:
         app.config['CASSANDRA_KEYSPACE'] = KEYSPACE_OVERRIDE
 
